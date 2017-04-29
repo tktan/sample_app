63d07e52ab671bffcf95456bedcd045bbfe88bf9
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-04-29 16:00:53 +0800

    rails db:migrate:reset

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,14 @@ ActiveRecord::Schema.define(version: 20170428052401) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
-    t.string   "remember_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "remember_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
