fb0e1f2c82abe43c2aea7458af90928905525f18
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-05-01 22:40:46 +0800

    Add PasswordResetsController#get_user before_action filter

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,6 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+
   def new
   end
 
@@ -16,5 +18,10 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  def get_user
+    @user = User.find_by(email: params[:email])
   end
 end
