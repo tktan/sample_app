a347a522be957ad9bf68a6cd38829679212631ec
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-05-01 22:59:22 +0800

    Add User#password_reset_expired?

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,6 +64,11 @@ class User < ApplicationRecord
     UserMailer.password_reset(self).deliver_now
   end
 
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
   # Creates and assigns the activation token and digest.
   def create_activation_digest
