0608d8147fbb2d12701d9bba436edd9b5e9e1cbc
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-05-01 22:48:00 +0800

    Add PasswordResetsController#user_params

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -22,6 +22,10 @@ class PasswordResetsController < ApplicationController
   end
 
   private
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
   def get_user
     @user = User.find_by(email: params[:email])
   end
