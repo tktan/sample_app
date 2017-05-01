acd0943a6fbab0b9786058edc0e9dc6f0e185a58
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-05-01 22:50:06 +0800

    Add PasswordResetsController#check_expiration before_action filter

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user,   only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -47,6 +48,14 @@ class PasswordResetsController < ApplicationController
   def valid_user
     unless @user && @user.activated? && @user.authenticated?(:reset, params[:id])
       redirect_to root_url
+    end
+  end
+
+  # Checks expiration of reset token.
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired."
+      redirect_to new_password_reset_url
     end
   end
 end
