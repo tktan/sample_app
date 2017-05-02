550afba31587445fe5a333ea75d04c47938e93c8
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-05-02 11:07:14 +0800

    Add user (1-m) microposts associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
+  has_many :microposts
+
   # Returns the hash digest of the given string
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
