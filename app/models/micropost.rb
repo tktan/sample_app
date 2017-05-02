df1980c409346e50b6f417b59e8e57c26fcfc53b
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-05-02 19:39:23 +0800

    Add Micropost#picture_size

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,4 +7,12 @@ class Micropost < ApplicationRecord
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+
+  private
+  # Validates the size of an uploaded picture.
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be <= 5MiB")
+    end
+  end
 end
