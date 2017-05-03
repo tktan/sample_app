10ab4bd33a589a3b6701dbdad1eebf0f228e3e2d
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-05-03 14:58:54 +0800

    Use key-value pairs in the feed's where method

--- app/models/user.rb
+++ app/models/user.rb
@@ -77,7 +77,7 @@ class User < ApplicationRecord
 
   # Returns a user's status feed.
   def feed
-    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    Micropost.where("user_id IN (:following_ids) OR user_id = :user_id", following_ids: following_ids, user_id: id)
   end
 
   # Follows a user.
