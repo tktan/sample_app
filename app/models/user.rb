9388856b3b584e6f8238028559a98f19e40e2bd6
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-05-03 12:39:40 +0800

    Implement user.followers using passive relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,9 @@ class User < ApplicationRecord
 
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   # Returns the hash digest of the given string
   def User.digest(string)
