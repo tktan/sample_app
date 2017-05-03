1ae7b3e21a066835f39154257b2ed68b8ae47eb0
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-05-03 14:14:05 +0800

    Update RelationshipsController#create

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,6 +2,9 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
   def create
+    user = User.find(params[:followed_id])
+    current_user.follow(user)
+    redirect_to user
   end
 
   def destroy
