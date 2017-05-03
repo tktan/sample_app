5cca8663b97c3328786a020f0b035f3dfeadae2e
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-05-03 12:30:31 +0800

    Add the User model following association

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
 
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   # Returns the hash digest of the given string
   def User.digest(string)
