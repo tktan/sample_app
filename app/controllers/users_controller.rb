28fd829bd71509fc6f201aba45d726852a731871
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-05-03 13:44:13 +0800

    Add UsersController#followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,13 @@ class UsersController < ApplicationController
     render "show_follow"
   end
 
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render "show_follow"
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
